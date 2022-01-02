ad1fd891b4e0302a86343b5256af8d8039e58653
EnriqueGS88 <enrique.gzs@gmail.com>
2022-01-02 19:05:09 +0100

    dark background

--- line_script.js
+++ line_script.js
@@ -8,6 +8,20 @@ const myLabels = ["January", "February", "March", "April", "May", "June", "July"
 const myData = [0, 10, 15, 20, 25, 30, 35];
 const myData2 = [5, 15, 20, 17, 28, 29, 40];
 
+// plugin to set background color at the chart
+
+const chartBackground = {
+  id: 'custom_canvas_background_color',
+  beforeDraw: (chart) => {
+    const ctx = chart.canvas.getContext('2d');
+    ctx.save();
+    ctx.globalCompositeOperation = 'destination-over';
+    ctx.fillStyle = 'darkGrey';
+    ctx.fillRect( 29, 35, chart.width-42, chart.height-96 );
+    ctx.restore();
+  }
+};
+
 
 var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
@@ -34,9 +48,11 @@ var chart = new Chart(ctx, {
     }
   ]},
 
+  plugins: [chartBackground],
+  
   options: {
     title: {
-      text: "Protocol Earned Fees",
+      text: "Protocol Earned Fees Daily",
       display: true,
       position: 'top',
       fontColor: 'green',
